edf643289309bab5bf1478fbbf7918d4e3e7b356
Jesus Garcia Manday <jmanday@gmail.com>
2016-11-23 03:09:56 +0100

    actualizado fichero PP para provisionamiento de Ubuntu

--- provision/puppet/mysql.pp
+++ provision/puppet/mysql.pp
@@ -1,32 +1,12 @@
 #Instalar mysql
 
-class mysql::server {
+class mysql {
+
+	exec { ‘mysql-install':
+		command => 'apt-get install mysql-server',
+		user => 'ubuntu',
+		environment => 'HOME=/home/ubuntu',
+		require => Package['curl']
+	}
   
-  package { "mysql-server": ensure => installed }
-  package { "mysql": ensure => installed }
-
-  service { "mysqld":
-    enable => true,
-    ensure => running,
-    require => Package["mysql-server"],
-  }
-
-  file { "/var/lib/mysql/my.cnf":
-    owner => "mysql", group => "mysql",
-    source => "puppet:///mysql/my.cnf",
-    notify => Service["mysqld"],
-    require => Package["mysql-server"],
-  }
- 
-  file { "/etc/my.cnf":
-    require => File["/var/lib/mysql/my.cnf"],
-    ensure => "/var/lib/mysql/my.cnf",
-  }
-
-  exec { "set-mysql-password":
-    unless => "mysqladmin -uroot -p$mysql_password status",
-    path => ["/bin", "/usr/bin"],
-    command => "mysqladmin -uroot password $mysql_password",
-    require => Service["mysqld"],
-  }
 }
