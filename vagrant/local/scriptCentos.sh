a17a095af45429c22f1b8d3a33c0e21bf7bb2b16
Jesus Garcia Manday <jmanday@gmail.com>
2016-12-13 10:01:27 +0100

    instalado vagrant en máquina local close #16

--- vagrant/local/scriptCentos.sh
+++ vagrant/local/scriptCentos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Updating repository
-sudo apt-get -y update
+sudo yum -y update
 
 # Installing Nodejs and npm
-sudo apt-get install curl
+sudo yum install curl
 sudo curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 sudo nvm install v0.10.16
-sudo apt-get install npm
+sudo yum install npm
 
 #Installing Express
 sudo npm install -g express
@@ -15,4 +15,4 @@ sudo npm install -g express
 # Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
 sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
-sudo apt-get -y install mysql-server mysql-client-core-5.5
+sudo yum -y install mysql-server mysql-client-core-5.5
